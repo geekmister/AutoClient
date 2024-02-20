ff469cd4b983be235348e5aa79921c172473e915
Geekmister <geekmister.mg@gmail.com>
2024-02-20 17:24:06 +0800

    refactor(*): Adjust structure
    - Add testcases package, because of extend function in the future. Ex: parser excel-file of test-cases... And as do that the project structure is easy and friendly.

---
+++ testcases/__init__.py
@@ -0,0 +1,9 @@
+"""
+@Project : web-auto-test-client.git
+@File    : __init__.py.py
+@IDE     : PyCharm
+@Author  : geek mister
+@Date    : 2024/2/20 17:14
+@MyHome  : https://github.com/geekmister
+@Desc    : Here, type your description about the .py file
+"""

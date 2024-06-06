1d8a92021a7c5228f16cc9bffde9a21d390ec690
Geekmister <geekmister.mg@gmail.com>
2024-06-06 10:58:49 +0800

    feat(*): temp

--- test_cases/login_module.py
+++ test_cases/login_module.py
@@ -1,10 +1,8 @@
 """
-@ClassName: name
-@Description: description
-@Class:
-    1. name: description
+@ClassName: login_module
+@Description: This module contains the test cases for login module.
 @Methods:
     1. name: description
 @Author: geekmister
-@Date: xxxx/xx/xx
+@Date: 2024/06/06
 """

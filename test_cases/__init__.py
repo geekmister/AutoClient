6c1fbd1bb00bd492425208109f37036f34bd8139
Geekmister <geekmister.mg@gmail.com>
2024-06-06 11:00:27 +0800

    feat(*): Init project

--- test_cases/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@Project : cd-auto-test-client.git
-@File    : __init__.py.py
-@IDE     : PyCharm
-@Author  : Geekmister
-@Date    : 2024/1/4 14:42
-@MyHome  : https://github.com/geekmister
-@Desc    : Init python file of the module what test_cases.
-"""
